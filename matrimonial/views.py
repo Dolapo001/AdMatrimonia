756c4b9ab59bdac1b24a04c539ec62b9508e02a7
Dolapo001 <adedolapovictoria927@gmail.com>
2025-07-11 23:49:23 +0100

    11th July, 2025- Matrimony Profile Crud functionalities

--- matrimonial/views.py
+++ matrimonial/views.py
@@ -65,4 +65,37 @@ class CreateProfileView(APIView):
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
+class UpdateProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = MatrimonyProfileSerializer
 
+    def put(self, request):
+        try:
+            profile = MatrimonyProfile.objects.get_by_user_id(request.user.id)
+            if not profile:
+                return Response({
+                    "status": False,
+                    "message": "User profile not found"
+                }, status=status.HTTP_404_NOT_FOUND)
+            serializer = self.serializer_class(data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    "status": True,
+                    "message": "User profile updated successfully",
+                    "data": serializer.data
+                }, status=status.HTTP_200_OK)
+            return Response({
+                "status": False,
+                "message": "An error occurred"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            logger.error(f"Error updating user's profile: {e}")
+            return Response({
+                "status": False,
+                "message": "An error occurred while updating user's profile.",
+                "error": str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class Delete

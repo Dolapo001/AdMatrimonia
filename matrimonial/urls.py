20d623809ae86e236c773f1e4e1e3a03078392df
Dolapo001 <adedolapovictoria927@gmail.com>
2025-07-14 22:00:02 +0100

    14th July, 2025

--- matrimonial/urls.py
+++ matrimonial/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('profile/create/', CreateProfileView.as_view(), name='create_user_profile'),
     path('profile/update/', UpdateProfileView.as_view(), name='update_user_profile'),
     path('profile/delete/', DeleteProfileView.as_view(), name='delete_user_profile'),
+    path('profiles/', MatrimonyProfileListView.as_view(), name='matrimonia_profiles'),
+    path('profiles/<str:user_id>/', MatrimonyProfileDetailView.as_view()),
+    path('profile/<str:user_id>/pictures/', MatrimonyProfilePicturesView.as_view()),
+    path('profile/pictures/', UploadProfilePictureView.as_view()),
+    path('profile/pictures/<str:picture_id>/', DeleteProfilePictureView.as_view()),
 ]

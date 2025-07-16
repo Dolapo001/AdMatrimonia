84ff2c250bad63ba0a4dc69956a2d06faa430f47
Dolapo001 <adedolapovictoria927@gmail.com>
2025-07-16 22:02:20 +0100

    16th July, 2025

--- matrimonial/urls.py
+++ matrimonial/urls.py
@@ -6,9 +6,15 @@ urlpatterns = [
     path('profile/create/', CreateProfileView.as_view(), name='create_user_profile'),
     path('profile/update/', UpdateProfileView.as_view(), name='update_user_profile'),
     path('profile/delete/', DeleteProfileView.as_view(), name='delete_user_profile'),
-    path('profiles/', MatrimonyProfileListView.as_view(), name='matrimonia_profiles'),
-    path('profiles/<str:user_id>/', MatrimonyProfileDetailView.as_view()),
-    path('profile/<str:user_id>/pictures/', MatrimonyProfilePicturesView.as_view()),
-    path('profile/pictures/', UploadProfilePictureView.as_view()),
-    path('profile/pictures/<str:picture_id>/', DeleteProfilePictureView.as_view()),
+
+    path('profiles/', MatrimonyProfileListView.as_view(), name='list_matrimony_profiles'),
+    path('profiles/<str:user_id>/', MatrimonyProfileDetailView.as_view(), name='matrimony_profile_detail'),
+
+    path('profile/<str:user_id>/pictures/', MatrimonyProfilePicturesView.as_view(), name='get_profile_pictures'),
+    path('profile/pictures/', UploadProfilePictureView.as_view(), name='upload_profile_picture'),
+    path('profile/pictures/<str:picture_id>/', DeleteProfilePictureView.as_view(), name='delete_profile_picture'),
+
+    path('preferences/', GetPreferenceView.as_view(), name='get_partner_preferences'),       # GET
+    path('preferences/create/', SetPreferenceView.as_view(), name='create_partner_preferences'),  # POST
+    path('preferences/update/', UpdatePreferenceView.as_view(), name='update_partner_preferences'),  # PUT
 ]

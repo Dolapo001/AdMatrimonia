da38cefa12a1e96fe8e2ddb0ec21ebf50740c097
Dolapo001 <adedolapovictoria927@gmail.com>
2025-07-19 09:45:21 +0100

    19th July, 2025: changed uuid usage from uuid 4 to uuid7

--- common/models.py
+++ common/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-from uuid import uuid4
+import uuid6
 
 
 class BaseModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid6.uuid7,
+        editable=False,
+        unique=True
+    )
     created = models.DateTimeField(auto_now_add=True, db_index=True, null=True)
     updated = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         abstract = True
         ordering = ("-created",)
+

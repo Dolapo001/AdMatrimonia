d058fed7aedb6ee98690b5ef4be50870ebf78975
Dolapo001 <adedolapovictoria927@gmail.com>
2025-07-24 20:18:52 +0100

    24th July, 2025:

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path("register/", RegistrationView.as_view(), name="sign-up"),
     path("login/", LoginView.as_view(), name="sign-in"),
+    path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
+    path("forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
 ]

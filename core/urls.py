b6ef205e1c11a962e4ed44eb68d64512b0afc7df
Dolapo001 <adedolapovictoria927@gmail.com>
2025-07-26 19:02:27 +0100

    26th July, 2025:

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 
+
+
+
 urlpatterns = [
     path("register/", RegistrationView.as_view(), name="sign-up"),
     path("login/", LoginView.as_view(), name="sign-in"),
